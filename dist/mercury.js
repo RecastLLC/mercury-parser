520798349ed5532657c006755b057b4b4227691d
zeke <ezequiel454@gmail.com>
2021-08-20 19:04:19 -0300

    fix nymag extractor

--- dist/mercury.js
+++ dist/mercury.js
@@ -1784,7 +1784,7 @@ var NYMagExtractor = {
     // Order by most likely. Extractor will stop on first occurrence
     selectors: ['div.article-content', 'section.body', 'article.article'],
     // Selectors to remove from the extracted content
-    clean: ['.ad', '.single-related-story'],
+    clean: ['.ad', 'aside', 'section.package-list', '.single-related-story', 'div.nym-image-collection', 'div.nym-image', 'div.divider_short', 'p.clay-paragraph:nth-child(2)', 'div.attribution'],
     // Object of tranformations to make on matched elements
     // Each key is the selector, each value is the tag to
     // transform to.
@@ -1810,10 +1810,13 @@ var NYMagExtractor = {
     selectors: ['h1.lede-feature-title', 'h1.headline-primary', 'h1']
   },
   author: {
-    selectors: ['.by-authors', '.lede-feature-author']
+    selectors: [['meta[name="author"]', 'value'], '.by-authors', '.lede-feature-author']
   },
   dek: {
     selectors: ['.lede-feature-teaser']
+  },
+  excerpt: {
+    selectors: ['h2.display-teaser', 'h2']
   },
   date_published: {
     selectors: [['time.article-timestamp[datetime]', 'datetime'], 'time.article-timestamp']
@@ -7600,6 +7603,10 @@ function _collectAllPages() {
   return _collectAllPages.apply(this, arguments);
 }
 
+var cleanStringNymag = function cleanStringNymag(text) {
+  return text.replace(/(\r\n|\n|\\n|\r)/g, '').replace(/\s+/g, ' ').trim();
+};
+
 var Mercury = {
   parse: function () {
     var _parse = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime.mark(function _callee(url) {
@@ -7742,7 +7749,7 @@ var Mercury = {
                 turndownService = new TurndownService();
                 result.content = turndownService.turndown(result.content);
               } else if (contentType === 'text') {
-                result.content = $.text($(result.content));
+                result.content = $.text($(Extractor.domain === 'nymag.com' ? cleanStringNymag(result.content) : result.content));
               }
 
               return _context.abrupt("return", _objectSpread({}, result, extendedTypes));
