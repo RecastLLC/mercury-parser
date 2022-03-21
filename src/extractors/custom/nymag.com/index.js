0cd0281aabd8437daf36565f77c2a28ec7d9f670
Doruk Güneş <dorukgunes94@gmail.com>
2022-03-21 22:39:38 +0300

    Remove p.clay-paragraph:nth-child(2) from nymag extractor and add tests

--- src/extractors/custom/nymag.com/index.js
+++ src/extractors/custom/nymag.com/index.js
@@ -5,7 +5,17 @@ export const NYMagExtractor = {
     selectors: ['div.article-content', 'section.body', 'article.article'],
 
     // Selectors to remove from the extracted content
-    clean: ['.ad', 'aside', 'section.package-list' ,'.single-related-story', 'div.nym-image-collection', 'div.nym-image', 'div.divider_short', 'p.clay-paragraph:nth-child(2)', 'div.attribution', 'p.clay-paragraph:last-of-type'],
+    clean: [
+      '.ad',
+      'aside',
+      'section.package-list',
+      '.single-related-story',
+      'div.nym-image-collection',
+      'div.nym-image',
+      'div.divider_short',
+      'div.attribution',
+      'p.clay-paragraph:last-of-type',
+    ],
 
     // Object of tranformations to make on matched elements
     // Each key is the selector, each value is the tag to
@@ -38,7 +48,11 @@ export const NYMagExtractor = {
   },
 
   author: {
-    selectors: [['meta[name="author"]', 'value'], '.by-authors', '.lede-feature-author'],
+    selectors: [
+      ['meta[name="author"]', 'value'],
+      '.by-authors',
+      '.lede-feature-author',
+    ],
   },
 
   dek: {
@@ -46,7 +60,7 @@ export const NYMagExtractor = {
   },
 
   excerpt: {
-    selectors: ['h2.display-teaser', 'h2']
+    selectors: ['h2.display-teaser', 'h2'],
   },
 
   date_published: {
